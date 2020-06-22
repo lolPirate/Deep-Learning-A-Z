1eff904a7aac2bc08951d1899d5e099ef4b4ef50
lolPirate <debamalyadawn21@gmail.com>
2020-06-22 17:08:36 +0530

    Completed Reduced Boltzman Machine

--- Volume 2 - Unsupervised Deep Learning/Part 5 - Boltzmann Machines (BM)/Section 19 - Building a Boltzmann Machine/rbm.py
+++ Volume 2 - Unsupervised Deep Learning/Part 5 - Boltzmann Machines (BM)/Section 19 - Building a Boltzmann Machine/rbm.py
@@ -80,30 +80,72 @@ class RBM():
     def __init__(self, nv, nh):
         self.nv = nv
         self.nh = nh
-        self.W = T.randn(self.nh, self.nv)
+        self.W = T.randn(self.nh, self.nv).to(device)
         # Probability of hidden node given visible node bias
-        self.phvb = T.randn(1, self.nh)
+        self.phvb = T.randn(1, self.nh).to(device)
         # Probability of visible node given hidden node bias
-        self.pvhb = T.randn(1, self.nv)
+        self.pvhb = T.randn(1, self.nv).to(device)
 
     def sample_h(self, x):
         wx = T.mm(x, self.W.t())
         activation = wx + self.phvb.expand_as(wx)
-        p_h_given_v = F.sigmoid(activation)
+        p_h_given_v = T.sigmoid(activation)
         return p_h_given_v, T.bernoulli(p_h_given_v)
 
     def sample_v(self, y):
         wy = T.mm(y, self.W)
         activation = wy + self.pvhb.expand_as(wy)
-        p_v_given_h = F.sigmoid(activation)
+        p_v_given_h = T.sigmoid(activation)
         return p_v_given_h, T.bernoulli(p_v_given_h)
     
     def train(self, v0, vk, ph0, phk):
-        self.W += T.mm(v0.t(), ph0) - T.mm(vk.t(), phk)
+        self.W += (T.mm(v0.t(), ph0) - T.mm(vk.t(), phk)).t()
         self.pvhb += T.sum((v0 - vk), 0)
         self.phvb += T.sum((ph0 - phk), 0)
 
 
+nv = len(training_set_matrix[0])
+nh =  100
+batch_size = 100
+rbm = RBM(nv, nh)
 
-    
+# Training the RBM
+nb_epochs = 10
+for epoch in range(1, nb_epochs + 1):
+    train_loss = 0
+    s = 0.0
+    for id_user in range(0, nb_users - batch_size, batch_size):
+        vk = training_set_matrix[id_user: id_user + batch_size]
+        v0 = training_set_matrix[id_user: id_user + batch_size]
+        ph0, _ = rbm.sample_h(v0)
+        for k in range(10):
+            _, hk = rbm.sample_h(vk)
+            _, vk = rbm.sample_v(hk)
+            vk[v0 < 0] = v0[v0 < 0]
+        phk, _ = rbm.sample_h(vk)
+        """ print(v0.t().shape)
+        print(ph0.shape)
+        print(vk.t().shape)
+        print(phk.shape)
+        print(rbm.W.t().shape) """
+        rbm.train(v0, vk, ph0, phk)
+        train_loss += T.mean(T.abs(v0[v0 >= 0] - vk[v0 >= 0]))
+        s += 1
+    print(f'Epoch:{epoch:>10.2f} | Training Loss:{train_loss/s:>00.2f}')
+
+# Testing th RBM
+
+test_loss = 0
+s = 0.0
+for id_user in range(nb_users):
+    v = training_set_matrix[id_user:id_user + 1]
+    vt = test_set_matrix[id_user:id_user + 1]
+    if len(vt[vt>=0]) > 0:
+        _, h = rbm.sample_h(v)
+        _, v = rbm.sample_v(h)
+        test_loss += T.mean(T.abs(vt[vt >= 0] - v[vt >= 0]))
+        s += 1
+print(f'Test Loss:{test_loss/s:>00.2f}')
+
+
 
